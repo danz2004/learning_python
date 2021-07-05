7ac4d44b9b11b00a48afeefa46d8c46a0df02ca6
danz2004 <danielzhu04@gmail.com>
2021-07-05 13:13:54 -0700

    update

--- gc.py
+++ gc.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-print("Hello world")
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
+count = 0
+for i in range(len(dna)):
+	if dna[i] == 'C' or dna[i] == 'G':
+		count += 1
+print(f'{count / len(dna) : .2f}')
