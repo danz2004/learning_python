3b3ff25d74db64e9a2bc69ffffa5e938c410c6be
danz2004 <danielzhu04@gmail.com>
2021-07-09 13:18:53 -0700

    update

--- gc_win1.py
+++ gc_win1.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
-count = 0
-window = ''
-for i in range(len(seq) - 10):
-	for j in range(i, i + 11):
-		window += seq[j]
+for i in range(len(seq) - w + 1):
+	count = 0
+	for j in range(i, i + w):
 		if seq[j] == 'G' or seq[j] == 'C':
 			count += 1
-	print(f'{i} {window} {(count / 11) : .4f}')
-	window = ""
-	count = 0;
+	print(f'{i} {seq[i:i+w]} {(count / w) : .4f}')
