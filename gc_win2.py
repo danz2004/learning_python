2eb7402ef47c9c7ce5574311792961e4828c63a9
danz2004 <danielzhu04@gmail.com>
2021-07-09 13:31:44 -0700

    commit

--- gc_win2.py
+++ gc_win2.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
+# Write a program that computes the GC fraction of a DNA sequence in a window
+# Window size is 11 nt
+# Output with 4 significant figures using whichever method you prefer
+# Use no nested loops. Instead, count only the first window
+# Then 'move' the window by adding 1 letter on one side
+# And subtracting 1 letter from the other side
+# Describe the pros/cons of this algorith vs. nested loops
 seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
 w = 11
 window = ""
-count = 0;
-for i in range(11):
+count = 0
+for i in range(w):
 	window += seq[i]
 	if seq[i] == 'G' or seq[i] == 'C':
 		count += 1
-print(f'{0} {window} {count / 11 : .4f}')
-for i in range(1, len(seq) - 10):
+print(f'{0} {window} {count / w : .4f}')
+for i in range(1, len(seq) - w + 1):
 	if window[0] == 'G' or window[0] == 'C':
 		count -= 1
-	if seq[i + 10] == 'G' or seq[i + 10] == 'C':
+	if seq[i + w - 1] == 'G' or seq[i + w - 1] == 'C':
 		count += 1
-	window = window[1:] + seq[i + 10]
-	print(f'{i} {window} {count / 11 : .4f}')
+	window = window[1:] + seq[i + w - 1]
+	print(f'{i} {window} {count / w : .4f}')
 	
