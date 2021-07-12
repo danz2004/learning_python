cd0faa8ba1ad115fb497a7f1d882b858d4976f99
danz2004 <danielzhu04@gmail.com>
2021-07-12 15:01:20 -0700

    update

--- stats.py
+++ stats.py
@@ -5,10 +5,14 @@ data = [3, 1, 4, 1, 5]
 data.sort()
 mean = 0
 sd = 0
+med = 0;
 for i in range(len(data)):
 	mean += data[i]
 mean /= len(data)
 for i in range(len(data)):
 	sd += ((mean - data[i]) ** 2)
-	print(sd)
-print(f'Count: {len(data)} \nMinimum: {data[0]} \nMaximum: {data[len(data) - 1]} \nMean: {mean : .2f} \nStd. dev: {math.sqrt(sd / len(data)) : .2f} \nMedian: {data[int(len(data) / 2)] : .2f}')
+if(len(data) % 2 == 0):
+	med = (data[len(data) // 2] + data[len(data) // 2 + 1]) / 2
+else:
+	med = data[len(data) // 2]
+print(f'Count: {len(data)} \nMinimum: {data[0]} \nMaximum: {data[len(data) - 1]} \nMean: {mean : .2f} \nStd. dev: {math.sqrt(sd / len(data)) : .2f} \nMedian: {med : .2f}')
