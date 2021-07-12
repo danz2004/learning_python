9a649ce0180434a597f587f0a98ad807e266da56
danz2004 <danielzhu04@gmail.com>
2021-07-12 14:19:56 -0700

    upate

--- birthday.py
+++ birthday.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import random
 shared = 0
-for i in range(10000):
+trial_number = 10000
+for i in range(trial_number):
 	birthdays = [0] * 365
 	for j in range(25):
 		date = random.randint(0, 364)
@@ -9,4 +10,4 @@ for i in range(10000):
 		if birthdays[date] == 2:
 			shared += 1
 			break
-print(f'{shared / 10000 : .3f}')
+print(f'{shared / trial_number : .3f}')
